ba759f2d39cc834b06331d2da39b44c6f230684b
Alpha1Guru <piuschukwujekwuchinemerem@gmail.com>
2023-05-05 00:55:26 +0100

    some file changes

--- Python_folder/function_maker/Verifiers/verifier2.py
+++ Python_folder/function_maker/Verifiers/verifier2.py
@@ -1,30 +1,34 @@
 #function to check for unwanted characters (non-number char)in text
-def check_unwanted(text,float = True):
+def check_unwanted(decimal,float=True):
     if float == True:#by default treats the text as a float
         numbers = ".0123456789"
     elif float == False: # treats the text as an integer  
         numbers = "0123456789"
-    points = 0 #number of decimal points
-
-    if text == "":#Prevents user from giving and empty string
-        result = True # result becomes true if text is an empty string
-        return result
+    unwanted = []
+    points = 0
     
-    elif text != True:# but if text is not an empty string
-        for i in range(len(text)):#Counts the number of decimal points in the string
-            if text[i]==".":
-                points+=1
-        for i in range(len(text)):# loops through the text 
-            if text[i] in numbers and points <= 1:# if the character is a number and decimal points are no more than one
-                result = False # result is false for now
-            else: # if more than one decimal points or a non number character
-                result = True # result becomes True
-                break #The loop breaks immediately when result becomes True
-        return result
+    #This code prevents the user from inputting nothing as an input
+    if decimal=="":
+        unwanted.append(True)
+     
+     # This code checks the number of decimal points  
+    for i in range(len(decimal)):
+        if decimal[i]==".":
+            points +=1
+     #This code is to prevent the occurrence of two decimal points       
+    if points >1:
+        unwanted.append(True)
+                
+    for i in range(len(decimal)):   
+         if decimal[i] not in numbers : #prevents characters that are not numbers to be recognized
+            unwanted.append(True)
+         else:
+            unwanted.append(False)#recognizes the rest
+    return unwanted
 
-def verifloat(decimal):  #fuction verifies for correct float or decimal numbers
+def verifloat(decimal): #fuction verifies for correct float or decimal numbers
     result=check_unwanted(decimal) #checks the text in the check_unwanted func
-    while result == True: # while the text failed the test
+    while True in result: #while the text failed the test
         if decimal == "":
             message = f"\nYou gave an EMPTY value!\tPlease I need a Decimal number: "
         elif decimal!= "":
@@ -34,15 +38,15 @@ def verifloat(decimal):  #fuction verifies for correct float or decimal numbers
     return decimal #returns the correct(ed) text
 
 def verint(integer): #function verifies for correct integers only!
-    result=check_unwanted(integer,False) #checks the text as an integer and not a float
-    while result == True: # while the text failed the test
+    result=check_unwanted(integer,float =False) #checks the text as an integer and not a float
+    while True in result: #while the text failed the test
         if integer == "":
             message = f"\nYou gave an EMPTY value!\tPlease I need an Integer"
         elif integer!= "":
             message = f"\n'{integer}' is NOT AN INTEGER!\tPlease I need an Integer: "
         integer=input(message) #it promts user for a correct integer
         result=check_unwanted(integer,float=False) #checks the text again untill correct
-    return integer #returns the correct(ed) text
+    return integer #returns the correctf(ed)text
 
 # while True:
 #    user = verifloat(input("Using Verifloat for float Give me a number: ").strip())
