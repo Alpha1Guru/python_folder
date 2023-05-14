f13521d250118bb0f388f17b866a86a0b20ebe24
Alpha1Guru <piuschukwujekwuchinemerem@gmail.com>
2023-05-14 08:51:25 +0800

    nothing

--- Python_folder/nothing.py
+++ Python_folder/nothing.py
@@ -1,2 +1,16 @@
-with open("C:/User/hp/Documents/Github/python_folder/Python_folder/text.txt") as pyfile:
-    print(pyfile)
+filename = "C:/Users/hp/Documents/Github/python_folder/Python_folder/verifier.py"
+for char in filename:
+    if char in ("\\"):
+        filename = filename.replace(char,"/")
+with open(filename) as pyfile:
+    file  = pyfile.read()
+print(file)
+
+# filename = "C:\Users\hp\Documents\Github\python_folder\Python_folder\\verifier.py"
+# for char in filename:
+#     if char in ("\\"):
+#         filename = filename.replace(char,"/")
+# print(filename)      
+# # with open(filename) as pyfile:
+# #     file  = pyfile.read()
+# # print(file)
