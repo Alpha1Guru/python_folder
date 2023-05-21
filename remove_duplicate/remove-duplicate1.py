107dae2349d588f8713994adc6de3e4be7a4f48d
Alpha1Guru <109897479+Alpha1Guru@users.noreply.github.com>
2023-05-21 18:50:43 +0100

    some changes

--- remove_duplicate/remove-duplicate1.py
+++ remove_duplicate/remove-duplicate1.py
@@ -1,25 +1,25 @@
 def remove_duplicate(data): 
     def rm_dp_list(list):    
-            unique=[]
-            for x in list:
-                if x not in unique:
-                    unique.append(x)
-            return unique   
+        unique=[]
+        for x in list:
+            if x not in unique:
+                unique.append(x)
+        return unique   
     def rm_dp_str(str):
-            def count_char(cha,string):
-                count = 0
-                for x in string:
-                    if cha == x:
-                        count+=1     
-                return count       
-            unique=str[::-1]
-            character = []
-            for char in unique:
-                if char not in character:
-                    character.append(char)
-                    if count_char(char,unique)>1:
-                        unique = unique.replace(char,"",count_char(char,unique)-1)
-            return unique[::-1]
+        def count_char(cha,string):
+            count = 0
+            for x in string:
+                if cha == x:
+                    count+=1     
+            return count       
+        unique=str[::-1]
+        character = []
+        for char in unique:
+            if char not in character:
+                character.append(char)
+                if count_char(char,unique)>1:
+                    unique = unique.replace(char,"",count_char(char,unique)-1)
+        return unique[::-1]
     if type(data)== type([1,2,3]): 
         return rm_dp_list(data)
     elif type(data)==type("str"):
