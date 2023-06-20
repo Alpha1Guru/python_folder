20e06390cd869c1f7bb7edc21ab5e6b8ace97aa3
Alpha1Guru <109897479+Alpha1Guru@users.noreply.github.com>
2023-06-20 18:32:53 +0100

    modified Python_Crash_Course/if_statement/ordinal_numbers.py

--- Python_Crash_Course/if_statement/ordinal_numbers.py
+++ Python_Crash_Course/if_statement/ordinal_numbers.py
@@ -1,5 +1,14 @@
 numbers = [1,2,3,4,5,56,77,10,34,22,13,]
-ordinal = []
+def getOrdinalNum(number):
+    if str(number)[-1] in ("1","2","3") and number not in range(10,20):
+        if str(number)[-1] == "1":
+            ordnum = str(number) + "st"
+        elif str(number)[-1] == "2":
+            ordnum = str(number) + "nd"
+        elif str(number)[-1] == "3":
+            ordnum = str(number) + "rd"
+    else:
+        ordnum = str(number) + "th"
+    return ordnum
 for number in numbers:
-    if str(number)[-1] in ("1","2","3"):
-        
+    print(getOrdinalNum(number))
