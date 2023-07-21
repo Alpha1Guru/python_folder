32f8344c54c77b3082da8de45e71846411e3398c
Alpha1Guru <109897479+Alpha1Guru@users.noreply.github.com>
2023-07-21 02:27:06 +0100

    a new function for calculating the union of two strings into a long one

---
+++ FunctionStore/longest/longest.py
@@ -0,0 +1,12 @@
+def longest(a1, a2):
+    longest_list = sorted(set(a1).union(set(a2)))
+    longest_str = ""
+    for char in longest_list:
+        longest_str += str(char)
+    return longest_str
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+print(longest(a, b), longest(a, b) == "abcdefklmopqwxy")
+a = "abcdefghijklmnopqrstuvwxyz"
+print(longest(a, a), longest(a, a) == "abcdefghijklmnopqrstuvwxyz")
