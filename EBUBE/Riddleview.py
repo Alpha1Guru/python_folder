a4663f629b6927275f1fa3df30e2ed10d45e7a86
Alpha1Guru <109897479+Alpha1Guru@users.noreply.github.com>
2024-03-12 07:17:42 +0100

    Added Ebube's workspace and Automate with python codes

---
+++ EBUBE/Riddleview.py
@@ -0,0 +1,13 @@
+print("Welcome to riddleview")
+personalInfo1 = input ("What is your name ?:")
+personalInfo2 = input("what is your surname:")
+print(personalInfo1+ personalInfo2)
+if personalInfo2 == "18" :
+    print("You are welcome to the next stage - QUESTIONING")
+elif personalInfo1 == "Chukwujekwu Ebube":
+    print("Hello master well since you programmed me you can run me cant you?")
+Qans = input("what flies in the night yet has no wings?a, witches b,bees c, clouds:")
+if Qans == "a":
+    print("Correct! you win adieos")
+else:
+    print("Apologies you lose sire!")
