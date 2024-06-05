ede4a363c93bc15872f1dd0ce4b9cf79cb172de7
Alpha1Guru <109897479+Alpha1Guru@users.noreply.github.com>
2024-06-05 03:38:56 +0100

    Moved codwars pratictes to a new folder

--- FunctionStore/Code_Wars/Count_the_smiley_faces2.py
+++
@@ -1,8 +0,0 @@
-import re
-def count_smileys(valid_smiley_re_pattern: str, faces: list ):
-    faces_combined = "_".join(faces)
-    num_of_smileys = len(re.findall(valid_smiley_re_pattern, faces_combined))
-    return num_of_smileys
-if __name__ == "__main__":
-    smiley_regex = r"[:;]([~-])?[)D]"
-    print(count_smileys(smiley_regex, [':)', ';(', ';}', ':-D']))
--- Python_Crash_Course/Functions/Album.py
+++ Python_Crash_Course/Functions/Album.py
@@ -14,9 +14,9 @@ def make_album(artist_name: str, album_title: str, no_of_tracks= ""):
     if no_of_tracks:
         music_album["Number of tracks"] = int(no_of_tracks)
     return music_album
-# franklin = make_album("franklin","my world need you")
-# print(franklin)
-# Hope = make_album("hope","you make my heart")
-# print(Hope)
-# jay_jay = make_album("jay jay","songs of revivals",no_of_tracks="6")
-# print(jay_jay)
+franklin = make_album("franklin","my world need you")
+print(franklin)
+Hope = make_album("hope","you make my heart")
+print(Hope)
+jay_jay = make_album("jay jay","songs of revivals",no_of_tracks="6")
+print(jay_jay)
